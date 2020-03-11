b881373414aaebe203b4b1ab10aba96170b48f89
wyatt <wppurking@gmail.com>
2020-03-11 18:12:10 +0800

    fix dataset/load sh scirpt to load all data

--- dataset.sh
+++ dataset.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-wget https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_2018_1.zip
+# 下载一份数据, 简单测试
+#wget https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_2018_1.zip
+
+for s in `seq 1987 2018`
+do
+for m in `seq 1 12`
+do
+wget https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${s}_${m}.zip
+done
+done
+
--- load.sh
+++ load.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# 如果只下载了单个数据, 手动 unzip 操作需要 sed -i 's/\.00//g'  xxx.csv 操作
 # 简化了数据将 .00 替换为 空
 for i in *.zip; do echo $i; unzip -cq $i '*.csv' | sed 's/\.00//g' | clickhouse-client --host=example-perftest01j --query="INSERT INTO ontime FORMAT CSVWithNames"; done
